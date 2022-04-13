153fd04d8cc78f90ad3e4dbea36045fab7c7b6ca
mbui2 <mbui2@hawk.iit.edu>
2022-04-13 11:51:31 -0500

    ad scripts

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-var platform = new H.service.Platform({
-	appid: 'n9BhwoF43uZ3bLxqDfaw'
-  apikey: 'OpslUQ2FwfE_6whPEWSaLDDMl2Z7A_AP7lb9QSw73Y4'
-});
-var defaultLayers = platform.createDefaultLayers();
-var map;
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.397, lng: 150.644},
-		zoom: 8
-	});
+function initMap(){
+    const place = {lat: -25.344, lng: 131};
+    const map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 3,
+        center: place,
+    });
+    const marker = new google.maps.Marker({
+        position: place,
+        map: map,
+    });
 }
+google.maps.event.addDomListener(window, 'load', initMap)
---
+++ js/script.js.bak
@@ -0,0 +1,12 @@
+var platform = new H.service.Platform({
+	appid: 'n9BhwoF43uZ3bLxqDfaw'
+  apikey: 'OpslUQ2FwfE_6whPEWSaLDDMl2Z7A_AP7lb9QSw73Y4'
+});
+var defaultLayers = platform.createDefaultLayers();
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+}
